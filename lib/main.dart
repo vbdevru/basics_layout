41fc58ddc94f4427197d3af703ae6c3440c4c045
vbdevru <vbdevru@mail.ru>
2022-07-10 13:42:36 +0300

    Stack and Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,22 @@ class Home extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Row(
+        body: Stack(
+          alignment: Alignment.center,
           children: [
-            Expanded(
-              flex: 4,
-              child: Image.network(
-                  'https://astromeridian.su/wp-content/uploads/2020/11/Y9WnhE.jpg'),
+            Icon(
+              Icons.airplay,
+              color: Colors.red,
+              size: 200,
             ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.red,
-                child: Text('1'),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.green,
-                child: Text('2'),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.blue,
-                child: Text('3'),
+            Positioned(
+              top: 50,
+              left: 30,
+              child: Text(
+                'TV',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
             ),
           ],
